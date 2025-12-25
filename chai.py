0cd08fb6197783415bd7b205f4a54d1cbe98e36b
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-12-25 16:12:11 +0600

    Add files via upload

---
+++ chai.py
@@ -0,0 +1,9 @@
+from hello_chai import chai
+
+chai( " Green Tea ")
+
+#               Errors Details
+# Traceback (most recent call last):
+  #File "H:\All programs here\Python\basic\basic2.py", line 1, in <module>
+    #from basic1 import basic2
+#ImportError: cannot import name 'basic2' from 'basic1' (H:\All programs here\Python\basic\basic1.py)
---
+++ hello_chai.py
@@ -0,0 +1,10 @@
+print( " Hello Coder ")
+
+# ( )
+
+def chai(n ):
+    print( n )
+    
+# chai( Lemon Tea)  # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+chai( "Lemon Tea ")
