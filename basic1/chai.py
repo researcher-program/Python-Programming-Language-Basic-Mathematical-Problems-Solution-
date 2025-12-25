6aeabf13dc816adefa5694e9b9ccc55d23482aa3
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-12-25 10:05:44 +0000

    My First Combination Python Program

---
+++ basic1/chai.py
@@ -0,0 +1,5 @@
+from hello_chai import chai
+
+chai( " Green Tea ")
+
+ #python: can't open file '/workspaces/Python-Programming-Language-Basic-Mathematical-Problems-Solution-/chai.py': [Errno 2] No such file or directory
