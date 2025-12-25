db943225f09fd4a7c612164228994021ed02abc2
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-12-26 01:20:27 +0600

    Add variables for different chai types
    Added additional chai variables for different tea types.

--- hello_chai.py
+++ hello_chai.py
@@ -7,4 +7,10 @@ def chai(n ):
     
 # chai( Lemon Tea)  # SyntaxError: invalid syntax. Perhaps you forgot a comma?
 
+
 chai( "Lemon Tea ")
+
+chai_one = "Lemu Tea"
+chai_two = " Ada Tea"
+chai_three = ' masala Tea'
+# chai_three ==> Attributes
