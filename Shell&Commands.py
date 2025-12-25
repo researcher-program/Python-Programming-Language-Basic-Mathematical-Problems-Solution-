4947dc15ccf5fc2e553419a327440d3bc719e783
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-12-26 01:22:03 +0600

    Add files via upload

---
+++ Shell&Commands.py
@@ -0,0 +1,41 @@
+        #   Commands :
+1.python
+2.ctrl+Z{to exit} or ctrl+D
+3.ls
+4.print( " Hello Coder ")
+5. " chai " * 4
+6.score=100
+>>>score
+>>>10
+7.import os
+>>>os.getcwd()
+>>>'H:\\All programs here\\Python\\basic'
+# 8.for c in "chai":
+#...print(c)
+#IndentationError: expected an indented block after 'for' statement on line 1
+9.for c in "chai":
+...     print(c)
+... (press "Enter")
+c
+h
+a
+i
+10.import sys
+>>> sys.platform
+'win32'
+11.import hello_chai
+ Hello Coder 
+Lemon Tea
+12.import chai
+ Green Tea
+13.hello_chai.chai("Mint Tea")
+  Mint Tea
+14.from importlib import reload
+>>>reload(hello_chai)
+Hello Coder 
+Lemon Tea
+<module 'hello_chai' from 'H:\\All programs here\\Python\\basic\\hello_chai.py'>
+>>> hello_chai.chai_one
+'Lemu Tea'
+>>>hello_chai.chai_three
+' masala Tea'
